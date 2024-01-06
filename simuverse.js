57f5edabd13863aaa2173dbe2371b0437da5933e
Rhett Hancock <hancockrhett@gmail.com>
2024-01-05 20:36:58 -0700

    Removed comments & whitespace

--- simuverse.js
+++ simuverse.js
@@ -1,12 +1,9 @@
 let canvas, context, frames = 0;
 
-// Game loop
 function gameLoop() {
     requestAnimationFrame(gameLoop);
-    // Update game state
     update();
-    // Render the game
-    render(context);
+    render();
 }
 
 function update() {
@@ -14,18 +11,13 @@ function update() {
 }
 
 function render() {
-    // Clear the canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
-
     context.font = "10px monospace";
     context.fillStyle = '#fff';
     context.fillText(`Frames: ${frames}`, 5, 10);
     context.fillText(`Height: ${canvas.height}`, 5, 25);
     context.fillText(`Width: ${canvas.width}`, 5, 40);
-
     frames++;
-
-    // Draw NPCs and other elements
 }
 
 function handleResize() {
@@ -36,10 +28,8 @@ function handleResize() {
 function main() {
     canvas = document.getElementById('sv_canvas');
     context = canvas.getContext('2d');
-    
     window.addEventListener('resize', handleResize);
     handleResize();
-
     gameLoop();
 }
 
