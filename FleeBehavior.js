c3e131fdfd45b405aa86ca1d2f128de78e12799f
Rhett Hancock <hancockrhett@gmail.com>
2024-01-07 12:50:49 -0700

    Created FleeBehavior class by refactoring NPC

--- FleeBehavior.js
+++ FleeBehavior.js
@@ -1,15 +1,41 @@
 class FleeBehavior {
-    constructor(fleeSpeed, fleeAcceleration, fleeMaxDuration) {
-        this.fleeSpeed = fleeSpeed;
+    constructor(fleeAcceleration = 0.002, fleeMaxDuration = 500) {
         this.fleeAcceleration = fleeAcceleration;
         this.fleeMaxDuration = fleeMaxDuration;
     }
-
-    flee(npc, player) {
-        // Implementation of fleeing action
+    flee(npc, threat) {
+        if (npc.isTargetPerceivable(threat)) {
+            this.lastThreatPosition = { x: threat.x, y: threat.y };
+            this.fleeDuration = this.fleeMaxDuration;
+        }
+        if (this.fleeDuration > 0) {
+            let fleeDirection = {
+                x: npc.x - this.lastThreatPosition.x,
+                y: npc.y - this.lastThreatPosition.y
+            };
+            fleeDirection = VectorUtils.setMagnitude(fleeDirection, npc.maxSpeed);
+            npc.velocity.x = VectorUtils.lerp(npc.velocity.x, fleeDirection.x, this.fleeAcceleration);
+            npc.velocity.y = VectorUtils.lerp(npc.velocity.y, fleeDirection.y, this.fleeAcceleration);
+            this.fleeDuration--;
+        } else {
+            this.lastThreatPosition = null;
+        }
+        npc.emotions.anxiety = Math.min(this.emotions.anxiety + 0.01, 100);
     }
-
+    hide(npc) {
+        // TODO: Implement NPC hiding
+    }
     checkBehind(npc) {
-        // Implementation of checking behind action
+        const rotationSpeed = 0.01;
+        let desiredAngle = Math.atan2(-npc.velocity.y, -npc.velocity.x);
+        let currentAngle = Math.atan2(npc.velocity.y, npc.velocity.x);
+        let newAngle = VectorUtils.lerp(currentAngle, desiredAngle, rotationSpeed);
+        this.velocity.x = Math.cos(newAngle) * this.maxSpeed;
+        this.velocity.y = Math.sin(newAngle) * this.maxSpeed;
+        let oppositeDirection = {
+            x: -this.velocity.x,
+            y: -this.velocity.y
+        };
+        this.velocity = VectorUtils.setMagnitude(oppositeDirection, this.maxSpeed);
     }
 }
