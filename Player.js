585da157c919f2b3df9b0bc1ec38b3a7c862b766
Rhett Hancock <hancockrhett@gmail.com>
2024-01-13 19:41:41 -0700

    Player is now round

--- Player.js
+++ Player.js
@@ -4,11 +4,15 @@ class Player {
         this.y = y;
         this.color = '#00ff00';
         this.size = 20;
+        this.radius = this.size / 2;
         this.speed = 2;
     }
     draw(context) {
-        context.fillStyle = this.color;
-        context.fillRect(this.x, this.y, this.size, this.size);
+        const { x, y, radius, color } = this;
+        context.beginPath();
+        context.arc(x + radius, y + radius, radius, 0, 2 * Math.PI);
+        context.fillStyle = color;
+        context.fill();
     }
     move(dx, dy) {
         this.x += dx * this.speed;
